95fbb83c4c6976191266b945f223ef9bc8ebe3db
nikzulhanis <nickzezyuz@gmail.com>
2025-05-30 12:42:07 +0800

    Create MainActivity.java

---
+++ app/src/main/java/com/krx18tv/MainActivity.java
@@ -0,0 +1,18 @@
+package com.krx18tv;
+
+import android.os.Bundle;
+import androidx.fragment.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_browse_fragment, new VideoGridFragment())
+                .commit();
+        }
+    }
+}
